ea679fe178293fb3c43b72643d55e6f315f4605e
Sili Guo <sili.guo.ucr@gmail.com>
2018-03-26 04:38:23 -0700

    try 2

--- CSC-18A/3.1 You Do It/src/paradiseInfo/TestInfo.java
+++
@@ -1,9 +0,0 @@
-//pg.125 You Do It >> part 2 
-package paradiseInfo;
-
-public class TestInfo {
-	public static void main(String[] args) {
-		System.out.println("Calling method from another class: ");
-		ParadiseInfo.displayInfo();
-	}
-}
